d924db4b9d4bfe6b826e381ba9b19ac2645f72b4
asdasd571 <kng955626@gmail.com>
2025-07-11 18:16:45 +0900

    :wrench: Chore[#] workflows 수정

--- src/main/java/com/sw/escort/global/controller/HealthController.java
+++ src/main/java/com/sw/escort/global/controller/HealthController.java
@@ -1,5 +1,6 @@
 package com.sw.escort.global.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +10,7 @@ public class HealthController {
     @Deprecated
     @GetMapping("/health")
     public String health() {
-        return "ㅎㅇ";
+        return HttpStatus.OK.toString();
     }
 
 }
